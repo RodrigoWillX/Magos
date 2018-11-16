8f49fca3e599aa0572a45ac4147f9778893274bb
Rodrigo Will <44774584+RodrigoWillX@users.noreply.github.com>
2018-11-16 09:37:19 -0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Mago.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+    class Mago
+    {
+        //Atributos 
+        public string nome;
+        public int mana, hp, velocidade, defesa, ataque;
+
+        //Metodo construtor
+        public Mago(string defineNome, int defineMana, int defineHp, int defineAtaque, int defineDefesa, int defineVelocidade)
+        {
+            nome = defineNome;
+            mana = defineMana;
+            hp = defineHp;
+            ataque = defineAtaque;
+            defesa = defineDefesa;
+            velocidade = defineVelocidade;
+            Console.WriteLine(nome + " foi criado!");
+        }
+        //Metodos
+        public void Atacar()
+        {
+            Console.WriteLine(nome + " atacou com forca " + ataque);
+        }
+
+        //Metodo defender com parametro inimigo(string)
+        public void Defender(string inimigo)
+        {
+            Console.WriteLine(nome + " defendeu o ataque de " + inimigo);
+        }
+
+        public int LancarMagia(string alvo)
+        {
+            int dano = ataque * mana;
+            return dano;
+        }
+
+        //Metodo que retorna uma string
+        //Parametro tipoMovimenjto (int)
+        public string Mover(int tipoMovimento)
+        {
+            //0 = caminhada
+            //1 = corrida
+            //2 = teleporte
+            string acao = "idle";
+            if(tipoMovimento == 0)
+            {
+                acao = nome + " caminhou!";
+            }
+            if(tipoMovimento == 1)
+            {
+                acao = nome + " correu!";
+            }
+            if(tipoMovimento == 2)
+            {
+                acao = nome + " se teleportou!";
+            }
+            return acao;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mago gandalf = new Mago("Gandalf", 100, 30, 60, 100, 200);
+            
+
+            //Chama metodo atacar do objeto Gandalf
+            gandalf.Atacar();
+            gandalf.Defender("Saruman");
+            gandalf.Defender("Mia");
+            gandalf.Mover(2);
+            Console.WriteLine(gandalf.Mover(2));
+
+            //metodo de lancar magia
+            gandalf.LancarMagia("Saruman");
+
+            Console.WriteLine("Dano da magia de Gandalf " + gandalf.LancarMagia("Saruman"));
+
+            //Cria objeto mago saruman
+            Mago saruman = new Mago("Saruman", 120, 40, 70, 80, 180);
+            
+
+            //Chama metodo atacar do objeto Saruman
+            saruman.Atacar();
+            saruman.Defender("Mia");
+            saruman.Defender("Gandalf");
+            saruman.Mover(0);
+            Console.WriteLine(saruman.Mover(0));
+
+            //Cria objeto mago Mia 
+            Mago mia = new Mago("Mia", 150, 90, 100, 120, 250);
+            
+
+            //Chama metodo atacar do objeto Saruman
+            mia.Atacar();
+            mia.Defender("Saruman");
+            mia.Defender("Gandalf");
+            mia.Mover(1);
+            Console.WriteLine(mia.Mover(1));
+
+            Console.ReadLine();
+        }
+    }
+}
